05689948c31f6e31be6df90bb7ea22437f125297
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-05-15 15:37:55 +0600

    finding a particular product from a database

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,14 +1,21 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { Products } from "../types/product.types";
 
 export const productController = (req : IncomingMessage, res : ServerResponse) =>{
-    
+
+    //  /products ---> /products/1234
     const url = req?.url ;
     const method = req?.method ;
 
+    //getting the id for the particular product information
+    const urlParts = url?.split('/');
+    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null;
+
     if(url === '/products' && method === 'GET'){
 
-        const products = readProduct();
+        const products = readProduct();  
+        // [{}, {}, {}]
         
         res.writeHead(200, {
             'content-type' : 'application/json'
@@ -17,6 +24,20 @@ export const productController = (req : IncomingMessage, res : ServerResponse) =
         res.end(JSON.stringify({
             message : 'This is the product route', 
             data : products
+        }))
+    } else if(id !== null && method === 'GET'){
+
+        const products = readProduct();
+        const product = products.find((p : Products) => p.id === id)
+
+        res.writeHead(200, {
+            'content-type' : 'application/json'
+        })
+
+        res.end(JSON.stringify({
+            message : 'Here we will be finding a particular product',
+            data : product,
+        
         }))
     }
 }
